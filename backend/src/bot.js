e97f4ff9ed90755390031a5cee8ceb0b14aa20d7
Sahil <sahilnanda1995@gmail.com>
2020-09-16 20:11:40 +0530

    added validator & nominator tweet

--- backend/src/bot.js
+++ backend/src/bot.js
@@ -52,6 +52,16 @@ async function getTopThreeBackedValidatorsByNominators() {
 	return result;
 }
 
+const getNominatorsData = async () => {
+	try {
+		return await axios.get(
+			"https://yieldscan-api.onrender.com/api/twitter/top-nominator"
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
 async function takeScreenShotOfTopThreeBackedValidatorsByNominators() {
 	const validator = await getTopThreeBackedValidatorsByNominators();
 	await takeScreenShot(
@@ -151,7 +161,7 @@ async function takeScreenShots() {
 
 	await takeScreenShot(
 		__dirname + "/images/" + validator.stashId + ".png",
-		"https://ys-topvalidator.surge.sh",
+		"https://polkabot-twitter.netlify.app/#/top-validator",
 		".konvajs-content",
 		true
 	);
@@ -173,7 +183,38 @@ async function takeScreenShots() {
 	return validator;
 }
 
-async function takeScreenShotsAndPostItOnTwitter() {
+async function takeNomScreenShots() {
+	//   console.log('test, start');
+	const nominator = await getNominatorsData();
+
+	console.log("takeNomScreenShots - nominator");
+	console.log(nominator.data);
+
+	await takeScreenShot(
+		__dirname + "/images/" + nominator.data.nomId + ".png",
+		"https://polkabot-twitter.netlify.app/#/top-nominator",
+		".konvajs-content",
+		true
+	);
+
+	// await takeScreenShot(
+	// 	__dirname + "/images/" + validator[1].stashId + ".png",
+	// 	"https://polkanalytics.com/#/kusama/validator/" + validator[1].stashId,
+	// 	".konvajs-content",
+	// 	true
+	// );
+
+	// await takeScreenShot(
+	// 	__dirname + "/images/" + validator[2].stashId + ".png",
+	// 	"https://polkanalytics.com/#/kusama/validator/" + validator[2].stashId,
+	// 	".konvajs-content",
+	// 	true
+	// );
+	//   console.log('test, end');
+	return nominator.data;
+}
+
+async function takeValidatorScreenShotsAndPostItOnTwitter() {
 	const validator = await takeScreenShots();
 	console.log("validator", validator);
 	fs.readdir(__dirname + "/images", (err, files) => {
@@ -181,14 +222,33 @@ async function takeScreenShotsAndPostItOnTwitter() {
 			throw err;
 		} else {
 			console.log("files", files);
-			const fileImages = validator.map((validator) => {
-				const id = files.find(
-					(imageName) => imageName.split(".")[0] === validator.stashId
-				);
-				return id;
-			});
+			const fileImages = files.find(
+				(imageName) => imageName.split(".")[0] === validator.stashId
+			);
 			console.log("fileImages", fileImages);
 			uploadImages(fileImages, validator);
+		}
+	});
+}
+
+async function takeNominatorScreenShotsAndPostItOnTwitter() {
+	const nominator = await takeNomScreenShots();
+	console.log("++++++++helo++++++++");
+	console.log("nominator");
+	console.log(nominator);
+	console.log("nominator", nominator);
+	console.log("nomId");
+	console.log(nominator.nomId);
+	fs.readdir(__dirname + "/images", (err, files) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log("files", files);
+			const fileImages = files.find(
+				(imageName) => imageName.split(".")[0] === nominator.nomId
+			);
+			console.log("fileImages", fileImages);
+			uploadNomImages(fileImages, nominator);
 		}
 	});
 }
@@ -196,8 +256,9 @@ async function takeScreenShotsAndPostItOnTwitter() {
 eraChange.on("newEra", async () => {
 	try {
 		console.log("era func start");
-		await takeScreenShotsAndPostItOnTwitter();
-		await postTopThreeBackedValidatorsByNominatorsOnTwitter();
+		await takeValidatorScreenShotsAndPostItOnTwitter();
+		await takeNominatorScreenShotsAndPostItOnTwitter();
+		// await postTopThreeBackedValidatorsByNominatorsOnTwitter();
 		console.log("era func end");
 	} catch (err) {
 		console.log(err);
@@ -266,7 +327,7 @@ async function takeScreenShot(
 	// await page.waitFor(2000);
 	// await page.click("[aria-label='Switch to dark mode']");
 	// await page.focus("body");
-	// await page.waitFor(2000);
+	await page.waitFor(5000);
 	await page.click("body");
 	await page.screenshot({ path: outputPath, fullPage: fullPage });
 	console.log("Screenshot taken!");
@@ -274,8 +335,35 @@ async function takeScreenShot(
 	await browser.close();
 }
 
-function uploadImages(images, validator) {
+async function uploadImages(images, validator) {
 	console.log("Opening the image...");
+
+	let actorIdentity =
+		validator.info[0].twitter !== null
+			? validator.info[0].twitter
+			: validator.info[0].display !== null
+			? validator.info[0].display.length > 11
+				? validator.info[0].display.slice(0, 5) +
+				  "..." +
+				  validator.info[0].display.slice(-5)
+				: validator.info[0].display
+			: validator.stashId.length > 11
+			? validator.stashId.slice(0, 5) + "..." + validator.stashId.slice(-5)
+			: validator.stashId;
+
+	console.log("actorIdentity");
+	console.log(actorIdentity);
+
+	actorIdentity = "abc";
+
+	const poolReward = validator.estimatedPoolReward.toFixed(3);
+	console.log(poolReward);
+
+	const subPoolReward = await convertCurrency(poolReward, "kusama").then(
+		(convertedValue) => convertedValue
+	);
+	console.log("subPoolReward");
+	console.log(subPoolReward);
 
 	function uploadImg(image) {
 		return new Promise((resolve, reject) => {
@@ -296,27 +384,17 @@ function uploadImages(images, validator) {
 	console.log("Uploading an image...");
 
 	Promise.all([
-		uploadImg(images[0]),
-		uploadImg(images[1]),
-		uploadImg(images[2]),
+		uploadImg(images),
+		// uploadImg(images[1]),
+		// uploadImg(images[2]),
 	])
 		.then((list) => {
 			console.log("list", list);
 			bot
 				.post("statuses/update", {
-					status: `Highest Pool Reward Validator Leaderboard for previous era on @kusamanetwork
-
-  🥇${parseFloat(validator[0].poolRewardWithCommission).toFixed(3)} KSM for ${
-						validator[0].name
-					}
-  🥈${parseFloat(validator[1].poolRewardWithCommission).toFixed(3)} KSM for ${
-						validator[1].name
-					}
-  🥉${parseFloat(validator[2].poolRewardWithCommission).toFixed(3)} KSM for ${
-						validator[2].name
-					}
-        
-See how much you could be earning on https://polkanalytics.com/`,
+					status: `${actorIdentity}'s pool was the highest earning validator pool for the previous era on kusamanetwork with ${poolReward} KSM ($${subPoolReward.toFixed(
+						2
+					)}) earned. See your potential earnings on https://yieldscan.onrender.com`,
 					media_ids: list,
 				})
 				.catch((err) => {
@@ -327,3 +405,96 @@ See how much you could be earning on https://polkanalytics.com/`,
 			console.log("err", err);
 		});
 }
+
+async function uploadNomImages(images, nominator) {
+	console.log("Opening the image...");
+
+	// let actorIdentity =
+	// 	validator.info[0].twitter !== null
+	// 		? `@${validator.info[0].twitter}`
+	// 		: validator.info[0].display !== null
+	// 		? validator.info[0].display.length > 11
+	// 			? validator.info[0].display.slice(0, 5) +
+	// 			  "..." +
+	// 			  validator.info[0].display.slice(-5)
+	// 			: validator.info[0].display
+	// 		: validator.stashId.length > 11
+	// 		? validator.stashId.slice(0, 5) + "..." + validator.stashId.slice(-5)
+	// 		: validator.stashId;
+
+	console.log("nominator");
+	console.log(nominator);
+	console.log("nomId");
+	console.log(nominator.nomId);
+
+	const actorIdentity =
+		nominator.nomId.length > 11
+			? nominator.nomId.slice(0, 5) + "..." + nominator.nomId.slice(-5)
+			: nominator.nomId;
+
+	console.log(actorIdentity);
+
+	const nomReward = nominator.nomEraReward.toFixed(3);
+	console.log(nomReward);
+
+	const subPoolReward = await convertCurrency(nomReward, "kusama").then(
+		(convertedValue) => convertedValue
+	);
+	console.log("subPoolReward");
+	console.log(subPoolReward);
+
+	console.log(images);
+
+	function uploadNomImg(image) {
+		return new Promise((resolve, reject) => {
+			let image_path = path.join(__dirname, "/images/" + image);
+			let imageFile = fs.readFileSync(image_path, { encoding: "base64" });
+			bot.post("media/upload", { media_data: imageFile }, (err, data, res) => {
+				if (err) {
+					reject(err);
+				} else {
+					console.log("Image successfully uploaded!");
+					console.log("data", data);
+					resolve(data.media_id_string);
+				}
+			});
+		});
+	}
+
+	console.log("Uploading an image...");
+
+	Promise.all([
+		uploadNomImg(images),
+		// uploadImg(images[1]),
+		// uploadImg(images[2]),
+	])
+		.then((list) => {
+			console.log("list", list);
+			bot
+				.post("statuses/update", {
+					status: `${actorIdentity} was the highest earning nominator for the previous era on kusamanetwork with ${nomReward} KSM ($${subPoolReward.toFixed(
+						2
+					)}) earned. See your potential earnings on https://yieldscan.onrender.com`,
+					media_ids: list,
+				})
+				.catch((err) => {
+					console.log("twit bot err", err);
+				});
+		})
+		.catch((err) => {
+			console.log("err", err);
+		});
+}
+
+const convertCurrency = async (value, network) => {
+	const res = await axios(
+		"https://api.coingecko.com/api/v3/simple/price?ids=kusama%2Cpolkadot&vs_currencies=usd"
+	);
+	const { kusama, polkadot } = res.data;
+	let convertedValue = null;
+	if (network === "kusama") {
+		convertedValue = value * kusama.usd;
+	}
+	if (network === "polkadot") convertedValue = value * polkadot.usd;
+	return convertedValue;
+};
